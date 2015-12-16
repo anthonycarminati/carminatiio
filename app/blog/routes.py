fbbb0276bdbef10356ed03866c626a9dbd2d847f
Anthony Carminati <anthony@carminati.io>
2015-12-15 20:25:18 -0600

    added subdomain

--- app/blog/routes.py
+++ app/blog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template
 from . import blog
 
 
-@blog.route('/blog')#, subdomain='blog')
+@blog.route('/blog', subdomain='blog')
 def blog_home():
     return render_template('blog/index.html')
 
