3c6f9115d71a441496a4681bac8f618d74069e01
Anthony Carminati <anthony@carminati.io>
2015-12-20 22:17:05 -0600

    added auth logic

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,26 @@
-from flask import render_template
+from flask import render_template, current_app, request, redirect, url_for, flash
+from flask.ext.login import login_user, logout_user, login_required
+from ..models import User
+from .forms import LoginForm
 from . import auth
 
 
-@auth.route('/auth')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.verify_password(form.password.data):
+            flash('Invalid email or password')
+            return redirect(url_for('.login'))
+        login_user(user, form.remember_me.data)
+        return redirect(request.args.get('next') or url_for('blog.index'))
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out')
+    return redirect(url_for('blog.index'))
