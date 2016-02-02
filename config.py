9db897ec01f1fcaff9b2a78843d9ddeb009eafc6
Anthony Carminati <anthony@carminati.io>
2016-02-02 13:06:02 -0600

    changed inheritance in config file

--- config.py
+++ config.py
@@ -3,29 +3,32 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    TALKS_PER_PAGE = 10
-
-
-class DevelopmentConfig(Config):
-    DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 't0p s3cr3t'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # MAIL VARIABLES
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_SSL = False
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    TALKS_PER_PAGE = 3
+    # DATABASE VARIABLES
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # APPLICATION VARIABLES
+    TALKS_PER_PAGE = 10
+
+
+class DevelopmentConfig(Config):
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 't0p s3cr3t'
 
 
 class TestingConfig(Config):
-    TESTING = True
+    pass
 
 
 class ProductionConfig(Config):
     pass
+
 
 config = {
     'development': DevelopmentConfig,
