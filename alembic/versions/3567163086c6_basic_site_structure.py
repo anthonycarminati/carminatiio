c706bc1f3e151fcb678e05a0af256431f590e8f0
Anthony Carminati <anthony@carminati.io>
2016-03-05 18:39:08 -0600

    added blog_user table

--- alembic/versions/3567163086c6_basic_site_structure.py
+++ alembic/versions/3567163086c6_basic_site_structure.py
@@ -14,10 +14,34 @@ depends_on = None
 
 from alembic import op
 import sqlalchemy as sa
+from datetime import datetime
 
 
 def upgrade():
-    pass
+    op.create_table(
+        'blog_comment'
+    )
+    op.create_table(
+        'blog_post'
+    )
+    op.create_table(
+        'blog_user',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('email', sa.String(64), index=True, unique=True),
+        sa.Column('username', sa.String(64), index=True, unique=True),
+        sa.Column('is_admin', sa.Boolean),
+        sa.Column('password_hash', sa.String(256)),
+        sa.Column('name', sa.String(64)),
+        sa.Column('location', sa.String(64)),
+        sa.Column('bio', sa.Text()),
+        sa.Column('member_since', sa.DateTime(), default=datetime.utcnow()),
+        sa.Column('avatar_hash', sa.String(64)),
+        sa.Column('remember_me', sa.Boolean),
+        sa.Column('is_confirmed', sa.Boolean)
+    )
+    op.create_table(
+        'blog_followers'
+    )
 
 
 def downgrade():
