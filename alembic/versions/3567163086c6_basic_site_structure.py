0558bb7fb96c1a2549f28d1d8cf2dba6de1ae587
Anthony Carminati <anthony@carminati.io>
2016-03-05 18:20:36 -0600

    cleaned up requirements and added new alembic revision

---
+++ alembic/versions/3567163086c6_basic_site_structure.py
@@ -0,0 +1,24 @@
+"""basic site structure
+
+Revision ID: 3567163086c6
+Revises: 
+Create Date: 2016-03-05 18:19:19.127983
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3567163086c6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
