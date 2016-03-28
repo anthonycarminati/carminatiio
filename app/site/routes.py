f88a47ce7aa2aa354051bfaeddc3d9119f3c093a
Anthony Carminati <anthony@carminati.io>
2016-03-27 22:16:02 -0500

    testing contact, design okay

--- app/site/routes.py
+++ app/site/routes.py
@@ -8,19 +8,15 @@ from ..models import Post
 
 @site.route('/')
 def index():
-    # post = Post.query.order_by(Post.post_date.desc()).first()
-    return render_template('site/index.html')
-
-@site.route('/contact', methods=['GET', 'POST'])
-def contact():
     form = ContactForm()
+    # post = Post.query.order_by(Post.post_date.desc()).first()
 
     if request.method == 'GET':
-        return render_template('site/contact.html', form=form)
+        return render_template('site/index.html', form=form)
     elif request.method == 'POST':
         if form.validate() == False:
             flash('All fields are required.')
-            return render_template('site/contact.html', form=form)
+            return render_template('site/index.html', form=form)
         elif form.validate() == True:
             msg = Message(form.subject.data, sender='noreply@carminati.io', recipients=['anthony@carminati.io'])
             msg.body = """
@@ -29,4 +25,25 @@ def contact():
               Message: {message}
               """.format(name=form.name.data, email=form.email.data, message=form.message.data)
             mail.send(msg)
-        return redirect(url_for('.contact'))
+        return render_template('site/index.html', form=form) #redirect(url_for('site.index'))
+
+
+# @site.route('/contact', methods=['GET', 'POST'])
+# def contact():
+#     form = ContactForm()
+#
+#     if request.method == 'GET':
+#         return render_template('site/contact.html', form=form)
+#     elif request.method == 'POST':
+#         if form.validate() == False:
+#             flash('All fields are required.')
+#             return render_template('site/contact.html', form=form)
+#         elif form.validate() == True:
+#             msg = Message(form.subject.data, sender='noreply@carminati.io', recipients=['anthony@carminati.io'])
+#             msg.body = """
+#               From: {name}
+#               Email Address: {email}
+#               Message: {message}
+#               """.format(name=form.name.data, email=form.email.data, message=form.message.data)
+#             mail.send(msg)
+#         return redirect(url_for('.contact'))
