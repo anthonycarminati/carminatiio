287501a26b137e9bd1a1ddae6edfe216000f23c7
Anthony Carminati <anthony@carminati.io>
2016-03-29 22:15:02 -0500

    altered routes - still seeing but when sending messsage

--- app/site/routes.py
+++ app/site/routes.py
@@ -6,7 +6,7 @@ from flask.ext.mail import Message
 from ..models import Post
 
 
-@site.route('/')
+@site.route('/', methods=['GET', 'POST'])
 def index():
     form = ContactForm()
     # post = Post.query.order_by(Post.post_date.desc()).first()
@@ -17,6 +17,7 @@ def index():
         if form.validate() == False:
             flash('All fields are required.')
             return render_template('site/index.html', form=form)
+            print 'VALIDATEION FAILED'
         elif form.validate() == True:
             msg = Message(form.subject.data, sender='noreply@carminati.io', recipients=['anthony@carminati.io'])
             msg.body = """
@@ -25,7 +26,8 @@ def index():
               Message: {message}
               """.format(name=form.name.data, email=form.email.data, message=form.message.data)
             mail.send(msg)
-        return render_template('site/index.html', form=form) #redirect(url_for('site.index'))
+        form = ContactForm()
+        return redirect(url_for('.index'), form=form)
 
 
 # @site.route('/contact', methods=['GET', 'POST'])
