12b11f08ac473b6559788bc13bba3dc8c6913b6f
Anthony Carminati <anthony@carminati.io>
2016-10-09 00:07:38 -0500

    added deploy script

--- scripts/eb_deploy.sh
+++ scripts/eb_deploy.sh
@@ -2,19 +2,16 @@
 #! /bin/bash
 
 SHA1=$1
+ENV=dev
 
 # Deploy image to Docker Hub
-docker push circleci/hello:$SHA1
+docker push anthonycarminati/carminatiio:$SHA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=hello-bucket
-
+EB_BUCKET=anthonycarminati-carminatiio
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$ENV/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name hello --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
