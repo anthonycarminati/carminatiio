f3624d634305f454d120d2c1c1346c65a21fac6c
Anthony Carminati <anthony@carminati.io>
2016-10-09 12:40:15 -0500

    altered dev script with command line feedback

--- scripts/eb_deploy_dev.sh
+++ scripts/eb_deploy_dev.sh
@@ -1,19 +1,29 @@
 # deploy.sh
 #! /bin/bash
 
+# SET ENVIRONMENT VARIABLES
 SHA1=$1
 ENV=dev
 APPLICATION_NAME=carminatiio
-
-# Deploy image to Docker Hub
-docker push anthonycarminati/carminatiio:$SHA1
-
-# Create new Elastic Beanstalk version
 EB_BUCKET=anthonycarminati-carminatiio
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$ENV/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name $APPLICATION_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+echo "Environment Variables have been set successfully"
 
-# Update Elastic Beanstalk environment to new version
+# DEPLOY IMAGE TO DOCKERHUB
+docker push anthonycarminati/carminatiio:$SHA1
+echo "Image successfully deployed to DockerHub"
+
+# CREATE DOCKERRUN FILE AND PUSH TO S3
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+echo "DockerRun file successfully created"
+
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$ENV/$DOCKERRUN_FILE
+echo "DockerRun file successfully pushed to S3"
+
+# CREATE EB APPLICATION VERSION
+aws elasticbeanstalk create-application-version --application-name $APPLICATION_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+echo "EB application version successfully created"
+
+# UPDATE ELASTIC BEANSTALK ENVIRONMENT TO LATEST VERSION
 aws elasticbeanstalk update-environment --environment-name $ENV --version-label $SHA1
+echo "EB environment successfully set to latest version"
